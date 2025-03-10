eebf5d007a5e1d4cf43e79e33dfe326aac852e79
5hseok <ohhs1010@gmail.com>
2025-03-11 02:25:36 +0900

    :recycle: refactor(chatcontroller): chat endpoint 변경

--- src/main/java/com/example/cloud/chat/controller/ChatController.java
+++ src/main/java/com/example/cloud/chat/controller/ChatController.java
@@ -2,13 +2,10 @@ package com.example.cloud.chat.controller;
 
 import com.example.cloud.chat.dto.ChatMessageDTO;
 import com.example.cloud.chat.service.RedisPublisher;
-import com.example.cloud.chat.service.RedisSubscriber;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -21,7 +18,7 @@ public class ChatController {
 
     private final RedisPublisher redisPublisher;
 
-    // /publish/chat/room으로 발행
+    // /publish/room으로 발행
     // /subscribe/chat/room/{studyName}으로 구독
     @MessageMapping("/room")
     public void sendMessage( @Valid ChatMessageDTO message) {
